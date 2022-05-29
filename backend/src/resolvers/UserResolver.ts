83b3a87bffb80b85efb4cdcbbc06e47be3bd0b3f
Matheus <msbdacosta@hotmail.com>
2022-05-29 16:17:18 -0300

    finished the back end

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,5 +1,6 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql"
 import { User } from "../models/User"
+import crypto from 'crypto'
 
 // query: buscar dados
 //mutation: criar, alterar ou deletar
@@ -17,7 +18,7 @@ import { User } from "../models/User"
     async createUser(
         @Arg('name') name: string
     ) {
-        const user = { id: '1', name }
+        const user = { id: crypto.randomUUID() , name }
 
         this.data.push(user)
 
